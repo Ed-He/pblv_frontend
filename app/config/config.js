99e8d794dc6d8cde71b7eabda7159d95c2f14b13
ed-he <eduard.hermus@outlook.com>
2023-11-08 11:28:36 +0100

    first commit

---
+++ app/config/config.js
@@ -0,0 +1,10 @@
+
+SipaPage.setConfig({
+    /* default layout for all pages */
+    default_layout: 'default',
+    /* specific layouts for some pages { <page-name>: <layout-name> } */
+    default_layouts: {
+       /* @example overwrites the layout for the page 'login-page' with layout 'mini-dialog' */
+       // 'login-page': 'mini-dialog'
+    }
+});
